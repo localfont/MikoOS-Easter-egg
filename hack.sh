b16f4dd20bb17862d2312ac38fa54ab17b3c3af4
localfont <83416665+localfont@users.noreply.github.com>
2026-04-17 20:00:37 +0300

    Create hack.sh

---
+++ hack.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "\033[1;32m" # Yeşil
+echo "[!] MikoOS Hacking Mode Activated..."
+echo "Searching for treats... [OK]"
+echo "Accessing tuna database... [GRANTED]"
+echo " /\___/\
+      (> • . •<) / Reading your RAM... It smells like fish!"
+echo "\033[0m"
