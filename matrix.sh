5cdedf27ec5b1f480c32cbfaea60915c91ce3be6
localfont <83416665+localfont@users.noreply.github.com>
2026-04-17 19:58:18 +0300

    Create matrix.sh

---
+++ matrix.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "\033[0;32m"
+echo "M i k o O S  -  T h e  M a t r i x"
+echo "0 1 0 M I K O 1 0 1"
+echo "1 0 1 0 1 0 1 0 1 0"
+echo "   /ᐠ     𓏵  "
+echo "≽(..◜ᴗ◝..)≼ - Follow the white kitten..."
+echo "\033[0m"
